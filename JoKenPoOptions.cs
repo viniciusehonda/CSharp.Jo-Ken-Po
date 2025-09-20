c0488ab2da8b2a413cc5718b51f87bd3efa91ca6
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-09-20 17:42:06 -0300

    Created a simple Jo Ken Po game

---
+++ JoKenPoOptions.cs
@@ -0,0 +1,8 @@
+namespace CSharp.Jo_Ken_Po;
+
+public enum JoKenPoOptions
+{
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using CSharp.Jo_Ken_Po;
+
+Console.WriteLine("Welcome to the CSharp Jo Ken Po");
+Random rnd = new Random();
+
+while (true)
+{
+    Console.WriteLine("Please choose an option:");
+    Console.WriteLine("1 - Rock");
+    Console.WriteLine("2 - Paper");
+    Console.WriteLine("3 - Scissors");
+    Console.WriteLine("4 - Exit");
+
+    string? choice = Console.ReadLine();
+
+    if (Enum.TryParse(choice, out JoKenPoOptions playerOption))
+    {
+        if (!Enum.IsDefined(playerOption))
+        {
+            break;
+        }
+
+        var opponentOption = (JoKenPoOptions)rnd.Next(1, 3);
+
+        Console.WriteLine("Rock, Paper, Scissors !");
+
+        if (playerOption == opponentOption)
+        {
+            Console.WriteLine("It's a draw !");
+            continue;
+        }
+
+        if (playerOption == JoKenPoOptions.Rock && opponentOption == JoKenPoOptions.Scissors
+        || playerOption == JoKenPoOptions.Paper && opponentOption == JoKenPoOptions.Rock
+        || playerOption == JoKenPoOptions.Scissors && opponentOption == JoKenPoOptions.Paper)
+        {
+            Console.WriteLine("You Won the game !");
+            continue;
+        }
+
+        Console.WriteLine("You Lost the game !");
+    }
+}
